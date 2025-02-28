766fd9560c3b5eccd30a1e406a3c61e135947d04
waltersilva5 <waltersilva5@outlook.com>
2025-02-27 22:59:36 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # wsi-xtts-audio-generator-api
+
+projeto utilizado como base:
+    https://github.com/daswer123/xtts-webui
+
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create a Python virtual environment
+#use python 3.9
+# python -m venv venv -p python3.10
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install other dependencies from requirements.txt
+pip install -r requirements.txt
+pip install torch==2.1.1+cu118 torchaudio==2.1.1+cu118 --index-url https://download.pytorch.org/whl/cu118
+
+echo "Install deepspeed for Linux for python 3.10.x and CUDA 11.8"
+python scripts/modeldownloader.py
+
+echo "Install complete."
